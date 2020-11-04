83fa5d26bdafd6bf9af364bcab9f2784f3770775
Douglas Kautzman <kautzmad63@gmail.com>
2020-11-04 14:26:43 -0500

    created the variables and getters and setters for accounts POJO

--- java/tenmo-server/src/main/java/com/techelevator/tenmo/accounts/model/Accounts.java
+++ java/tenmo-server/src/main/java/com/techelevator/tenmo/accounts/model/Accounts.java
@@ -1,5 +1,36 @@
 package com.techelevator.tenmo.accounts.model;
 
 public class Accounts {
+	
+	private Long accountId;
+	private Long userId;
+	private double balance;
+	
+	
+	
+	
+	
+	
+	
+	public Long getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(Long accountId) {
+		this.accountId = accountId;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
 
 }
